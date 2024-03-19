ff9f51effac9b4b70400c2a90c95df71e37be37e
VIVIAN_N <VIVIAN_N@HOTMAIL.CO.UK>
2024-03-19 17:17:10 +0000

    completed half of mvp

---
+++ wainwrights_start_code/app.js
@@ -0,0 +1,34 @@
+
+// Getting all the Wainwrights
+const getAllWainwrights = async () =>{
+    return fetch('https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json')
+    .then(response => response.json());
+
+    // const response = await fetch('https://raw.githubusercontent.com/annahndr/annahndr.github.io/master/wainwrights_data/wainwrights.json');
+    // const jsonData = await response.json();  
+}
+
+
+const wainwrightsContainer = document.createElement("div");
+
+
+
+document.querySelector("body").appendChild(wainwrightsContainer);
+
+
+const button = document.querySelector("button")
+button.addEventListener("click", () => {
+    getAllWainwrights();
+});
+
+
+document.getElementById('wainwrightsData').addEventListener('click', async() =>{
+    const results = await getAllWainwrights();
+    results.forEach((result) =>{
+       const paragraph = document.createElement("p");
+       paragraph.innerText = result.name
+       wainwrightsContainer.appendChild(paragraph);
+    })
+})
+
+
